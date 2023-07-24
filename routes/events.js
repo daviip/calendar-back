082e6668d833fc6318826fff6792d1715c279298
David Padial <david.padial@aktios.com>
2023-07-24 17:43:24 +0200

    Backend 1.0 terminado

---
+++ routes/events.js
@@ -0,0 +1,31 @@
+const { Router } = require("express");
+const { validarJWT } = require("../middlewares/validar-jwt");
+const {
+  getEventos,
+  crearEvento,
+  actualizarEvento,
+  eliminarEvento,
+} = require("../controllers/events");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
+const { isDate } = require("../helpers/isDate");
+
+const router = Router();
+
+router.use(validarJWT);
+
+router.get("/", getEventos);
+router.post(
+  "/",
+  [
+    check("title", "El titulo es obligatorio").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatorio").custom(isDate),
+    check("end", "Fecha de finalización es obligatorio").custom(isDate),
+    validarCampos,
+  ],
+  crearEvento
+);
+router.put("/:id", actualizarEvento);
+router.delete("/:id", eliminarEvento);
+
+module.exports = router;
